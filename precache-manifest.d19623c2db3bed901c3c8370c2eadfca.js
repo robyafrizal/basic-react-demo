9d7f37409474a6a99c0997d352e57521bffc9c75
herdanuyp <heh.danu@gmail.com>
2019-11-15 15:18:16 +0700

    Updates

---
+++ precache-manifest.d19623c2db3bed901c3c8370c2eadfca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bbf11c49b3c2307d1bd37a9a4ae661a",
+    "url": "/basic-react-demo/index.html"
+  },
+  {
+    "revision": "6fab89153dc7755a8a0e",
+    "url": "/basic-react-demo/static/css/main.6fb2a0f6.chunk.css"
+  },
+  {
+    "revision": "a52fa2e3d750a5559fc2",
+    "url": "/basic-react-demo/static/js/2.c202c282.chunk.js"
+  },
+  {
+    "revision": "6fab89153dc7755a8a0e",
+    "url": "/basic-react-demo/static/js/main.a116d389.chunk.js"
+  },
+  {
+    "revision": "1fa96560f89af1968d4e",
+    "url": "/basic-react-demo/static/js/runtime~main.3738e85d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/basic-react-demo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1fca2f93957f6ec920cefdb4714f291d",
+    "url": "/basic-react-demo/static/media/maher.1fca2f93.m4a"
+  }
+]);
