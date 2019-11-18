19d2a9e67ccf40f8149b655ace92cce26e3d92d2
herdanuyp <heh.danu@gmail.com>
2019-11-18 10:18:41 +0700

    Create Tic Tac Toe

--- src/App.js
+++ src/App.js
@@ -1,37 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Game from './components/Game';
 import './App.css';
-import MaherMusic from './assets/maher.m4a';
 
-import { DisplayInformation } from './components/displayInformation';
-
-//STATEFUL COMPONENT
-//PARENT COMPONENT
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      information: 'Edit src/App.js and save to reload.'
-    };
-  }
   render() {
     return (
       <div className='App'>
-        <audio src={MaherMusic} autoPlay />
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-
-          {/* CHILD COMPONENT */}
-          <DisplayInformation information={this.state.information} />
-
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'>
-            Incredible Igneel
-          </a>
-        </header>
+        <h1>Tic Tac Toe</h1>
+        <Game />
       </div>
     );
   }
